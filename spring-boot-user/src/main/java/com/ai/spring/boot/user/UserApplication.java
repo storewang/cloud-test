163f423a77cc23932c247ea666fc8fdb9f3793c4
storewang <wxy0308beijin@126.com>
2020-05-09 15:16:56 +0800

    dev_spring-boot-cloud#@调整配置

--- spring-boot-user/src/main/java/com/ai/spring/boot/user/UserApplication.java
+++ spring-boot-user/src/main/java/com/ai/spring/boot/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.ai.spring.boot.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  **/
 @SpringBootApplication
+@Slf4j
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
+        log.info("================>{}",System.getProperty("spring.profiles.active","没有获取到"));
     }
 }
